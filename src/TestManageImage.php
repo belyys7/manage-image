83a5b1dc31c28d1ff6c01d7699c56deb67ab4fa0
belyylion <sergey.belyy.lion@gmail.com>
2020-01-26 02:34:01 +0300

    Initial commit

---
+++ src/TestManageImage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace belyys7\Tools;
+
+class TestManageImage
+{
+    private $name;
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
